7fb2ee740896f5d925fabcd0ad21583a887ba5e7
Robert Ha <rroberthha@gmail.com>
2019-05-05 13:38:45 -0700

    third site added smooth scrolling and appearing text after scrolling down

--- third/app.js
+++ third/app.js
@@ -1,17 +1,40 @@
 var head;
 var posy;
+var top, projects, about;
 window.onload = function(){
     head = document.querySelector('header');
+    top = document.querySelector('.top').offsetHeight;
+    projects = document.querySelector('.projects').offsetHeight;
+    about = document.querySelector('.about').offsetHeight;
     posy = 0;
 };
 
 
 window.addEventListener('scroll', function(){
+    check();
+});
+
+window.addEventListener('onmousedown', function(){
+    check();
+});
+
+check = function(){
     if(window.scrollY > posy){
         head.style.top = '-'+head.offsetHeight+'px';
     }
     else{
         head.style.top = '0px';
     }
+
+    if(posy > top.innerHeight / 6){
+        document.querySelector('.projects').style.opacity = 1.0;
+    }
+    if(posy > top.innerHeight + projects/2){
+        document.querySelector('.about').style.opacity = 1.0;
+    }
+    if(posy > top.innerHeight + projects + about/2){
+        document.querySelector('.contact').style.opacity = 1.0;
+    }
+
     posy = window.scrollY;
-});
+};
